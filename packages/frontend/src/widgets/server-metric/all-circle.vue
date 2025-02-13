d77bdbd411cb62af1edb693020806838fb46e521
hos1miya <113900368+hos1miya@users.noreply.github.com>
2025-02-14 01:07:16 +0900

    サーバーメトリクスウィジェット更新
    ページ追加

---
+++ packages/frontend/src/widgets/server-metric/all-circle.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="vrvdvrys">
+	<XPie class="pie" :value="cpuUsage" :title="`CPU`"/>
+	<XPie class="pie" :value="memUsage" :title="`MEM`"/>
+	<XPie class="pie" :value="diskUsage" :title="`DISK`"/>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, onBeforeUnmount, ref } from 'vue';
+import * as Misskey from 'misskey-js';
+import XPie from './pie.vue';
+
+const props = defineProps<{
+	connection: Misskey.ChannelConnection<Misskey.Channels['serverStats']>,
+	meta: Misskey.entities.ServerInfoResponse
+}>();
+
+const cpuUsage = ref<number>(0);
+const memUsage = ref<number>(0);
+const diskUsage = ref<number>(0);
+
+function onStats(stats) {
+	cpuUsage.value = stats.cpu;
+	memUsage.value = stats.mem.active / props.meta.mem.total;
+	diskUsage.value = props.meta.fs.used / props.meta.fs.total;
+}
+
+onMounted(() => {
+	props.connection.on('stats', onStats);
+});
+
+onBeforeUnmount(() => {
+	props.connection.off('stats', onStats);
+});
+</script>
+
+	<style lang="scss" scoped>
+	.vrvdvrys {
+		display: flex;
+		padding: 16px;
+
+		> .pie {
+			height: 82px;
+			flex-shrink: 0;
+			margin-right: 12px;
+		}
+	}
+	</style>
