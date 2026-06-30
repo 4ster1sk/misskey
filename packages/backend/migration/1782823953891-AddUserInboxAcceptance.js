052bb5c44c10b6f29ccb1974e87fdfbd4aee3801
4sterisk <4sterisk@ymail.ne.jp>
2026-06-30 16:38:30 +0000

    fix

--- packages/backend/migration/1782823953891-AddUserInboxAcceptance.js
+++ packages/backend/migration/1782823953891-AddUserInboxAcceptance.js
@@ -8,6 +8,7 @@ export class AddUserInboxAcceptance1782823953891 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" ADD "inboxAcceptance" character varying(16) NOT NULL DEFAULT 'all'`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."inboxAcceptance" IS 'The inbox acceptance policy for this user.'`);
     }
 
     async down(queryRunner) {
